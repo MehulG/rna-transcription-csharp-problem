c431514a315795c74b790ceb982cc12e364c202d
mehul.gaidhani <mehul.gaidhani@cgi.com>
2019-08-29 15:51:55 +0530

    done

--- RNATranscription/RNATranscription.cs
+++ RNATranscription/RNATranscription.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace RNATranscriptionModule
 {
@@ -6,7 +8,32 @@ namespace RNATranscriptionModule
     {
         public static string ToRna(string nucleotide)
         {
-            throw new NotImplementedException("You need to implement this function.");
-        }
+
+            Dictionary<char, char> dict = new Dictionary<char, char>();
+            dict.Add('G', 'C');
+            dict.Add('C', 'G');
+            dict.Add('T', 'A');
+            dict.Add('A', 'U');
+
+            char[] nuc_array = nucleotide.ToCharArray();
+            List<char> rna_list= new List<char>();
+            string rna = "";
+            foreach(char ch in nuc_array){
+                //Console.WriteLine(dict[ch]);
+                //rna_list.Add(dict[ch]);
+                rna += dict[ch];
+            }
+            //rna_list.Add('A');
+            //foreach(var x in rna_list){
+            //Console.WriteLine(x);
+//}
+            //string rna = rna_list.ToString();
+            //Console.WriteLine(rna);
+            return rna;        
+}
+        public static void Main(string[] args){
+            ToRna("ACGTGGTCTTAA");
+}
+
     }
 }
